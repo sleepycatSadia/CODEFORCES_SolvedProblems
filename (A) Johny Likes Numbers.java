c9a657856aa1c4746d0022a7554e1745d5ab9416
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2021-11-06 02:03:15 +0600

    Add files via upload

---
+++ (A) Johny Likes Numbers.java
@@ -0,0 +1,198 @@
+import java.io.*;
+import java.nio.charset.Charset;
+public class Main {
+  public static void main(String[] args) throws java.io.IOException{
+    Charset charset=Charset.forName("ascii");
+    FastIO sc=new FastIO(System.in,System.out,charset);
+    int n=sc.readInt();
+    int k=sc.readInt();
+    n=n+1;
+    long s=n;
+    if(n%k!=0){
+      n--;
+      if(n==k)
+        s=2*k;
+      else if(n>k)
+        s=(k*(n/k))+k;
+      else
+        s=k;
+    }
+    System.out.println(s);
+  }
+}
+class FastIO {
+  public final StringBuilder cache = new StringBuilder(1 << 20);
+  private final InputStream is;
+  private final OutputStream os;
+  private final Charset charset;
+  private StringBuilder defaultStringBuf = new StringBuilder(1 << 8);
+  private byte[] buf = new byte[1 << 13];
+  private int bufLen;
+  private int bufOffset;
+  private int next;
+  
+  public FastIO(InputStream is, OutputStream os, Charset charset) {
+    this.is = is;
+    this.os = os;
+    this.charset = charset;
+  }
+  
+  public FastIO(InputStream is, OutputStream os) {
+    this(is, os, Charset.forName("ascii"));
+  }
+  
+  private int read() {
+    while (bufLen == bufOffset) {
+      bufOffset = 0;
+      try {
+        bufLen = is.read(buf);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      if (bufLen == -1) {
+        return -1;
+      }
+    }
+    return buf[bufOffset++];
+  }
+  
+  public void skipBlank() {
+    while (next >= 0 && next <= 32) {
+      next = read();
+    }
+  }
+  
+  public int readInt() {
+    int sign = 1;
+    
+    skipBlank();
+    if (next == '+' || next == '-') {
+      sign = next == '+' ? 1 : -1;
+      next = read();
+    }
+    
+    int val = 0;
+    if (sign == 1) {
+      while (next >= '0' && next <= '9') {
+        val = val * 10 + next - '0';
+        next = read();
+      }
+    } else {
+      while (next >= '0' && next <= '9') {
+        val = val * 10 - next + '0';
+        next = read();
+      }
+    }
+    
+    return val;
+  }
+  
+  public long readLong() {
+    int sign = 1;
+    
+    skipBlank();
+    if (next == '+' || next == '-') {
+      sign = next == '+' ? 1 : -1;
+      next = read();
+    }
+    
+    long val = 0;
+    if (sign == 1) {
+      while (next >= '0' && next <= '9') {
+        val = val * 10 + next - '0';
+        next = read();
+      }
+    } else {
+      while (next >= '0' && next <= '9') {
+        val = val * 10 - next + '0';
+        next = read();
+      }
+    }
+    
+    return val;
+  }
+  
+  public double readDouble() {
+    boolean sign = true;
+    skipBlank();
+    if (next == '+' || next == '-') {
+      sign = next == '+';
+      next = read();
+    }
+    
+    long val = 0;
+    while (next >= '0' && next <= '9') {
+      val = val * 10 + next - '0';
+      next = read();
+    }
+    if (next != '.') {
+      return sign ? val : -val;
+    }
+    next = read();
+    long radix = 1;
+    long point = 0;
+    while (next >= '0' && next <= '9') {
+      point = point * 10 + next - '0';
+      radix = radix * 10;
+      next = read();
+    }
+    double result = val + (double) point / radix;
+    return sign ? result : -result;
+  }
+  
+  
+  public int readLine(char[] data, int offset) {
+    int originalOffset = offset;
+    while (next != -1 && next != '\n') {
+      data[offset++] = (char) next;
+      next = read();
+    }
+    return offset - originalOffset;
+  }
+  
+  public int readString(char[] data, int offset) {
+    skipBlank();
+    
+    int originalOffset = offset;
+    while (next > 32) {
+      data[offset++] = (char) next;
+      next = read();
+    }
+    
+    return offset - originalOffset;
+  }
+  
+  public int readString(byte[] data, int offset) {
+    skipBlank();
+    
+    int originalOffset = offset;
+    while (next > 32) {
+      data[offset++] = (byte) next;
+      next = read();
+    }
+    
+    return offset - originalOffset;
+  }
+  
+  public char readChar() {
+    skipBlank();
+    char c = (char) next;
+    next = read();
+    return c;
+  }
+  
+  public void flush() {
+    try {
+      os.write(cache.toString().getBytes(charset));
+      os.flush();
+      cache.setLength(0);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public boolean hasMore() {
+    skipBlank();
+    return next != -1;
+  }
+}
